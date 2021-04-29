a0fc41c81257140a0f55880ec55c7d11a006f45c
Dominique Sakrisson <domsakrisson@gmailcom>
2021-04-28 20:17:26 -0700

    mapping slack id to balances

--- contracts/SlackerToken.sol
+++ contracts/SlackerToken.sol
@@ -1,82 +1,103 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
-pragma experimental ABIEncoderV2;
 
 import './SafeMath.sol';
 
 contract SlackerToken {
     using SafeMath for uint256;
 
-    string[] slackers;
-    mapping(string => uint) balances;
-
+    string[] public slackers;
+    string public name = 'Slacker Token';
+    string public symbol = 'ST';
+    string public standard = 'ST token v1.0.0';
     uint256 public decimal = 0;
     uint256 public totalSupply;
 
     event Transfer(
-        string indexed _from,
-        string indexed _to,
+        address indexed _from,
+        address indexed _to,
         uint256 _value
     );
 
-    // event Approval(
-    //     address indexed _owner,
-    //     address indexed _spender,
-    //     uint256 _value
-    // );
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
 
-  
-    // function addUser(string memory slackId) public pure returns(string memory) {
-    //     return slackId;
-    // }
- 
     constructor(uint256 _initialSupply) public{
-        // balances[slackers[0]] = _initialSupply;
+        balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
+    }
+
+    mapping(address => uint256) public balanceOf;
+    mapping(string => address) public _slackId;
+    mapping(address => mapping(address => uint256)) public allowance;
+    // mapping(string => mapping(address => uint256)) public _slackId;
+    // mapping(address => user) users;
+
+    // struct user {
+	// 	string slackId;
+    // }
+
+    // function addUser(string memory _slackId) public pure returns(string memory) {
+    //     return _slackId;
+    // }
+
+    function userBalance(address userAddress) public view returns(uint){
+        return balanceOf[userAddress];
+    }
+    function addUserFunds(address userAddress, uint _amount) public{
+        balanceOf[userAddress] += _amount;
     }
 
     function getName() public pure returns(string memory) {
         return 'Slacker Token';
-    }
-     function getSymbol() public pure returns(string memory) {
-        return 'ST';
+
     }
 
-    function getTotalSupply() public view returns(uint256){
-        return  totalSupply;
+    function setSlacker(string memory _slackerId) public returns (bool success) {
+        slackers.push(_slackerId);
+        return true;
     }
 
 
-    // function approve(address _spender, uint256 _value) public returns(bool success) {
+    function transfer(address _to, uint256 _value) public returns(bool success) {
 
-    //     allowance[msg.sender][_spender] = _value;
+    require(balanceOf[msg.sender] >= _value);
+    
+    balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+    balanceOf[_to] = balanceOf[_to].add(_value);
+    
+    emit Transfer(msg.sender, _to, _value);
+    
+    return true;
 
-    //     emit Approval(msg.sender, _spender, _value);
+    }
 
-    //     return true;
-    // }
+    function approve(address _spender, uint256 _value) public returns(bool success) {
 
+        allowance[msg.sender][_spender] = _value;
 
-    function transferFrom(string memory _from, string memory _to, uint256 _value) public returns (bool success) {
-
-        require(_value <= balances[_from]);
-
-        // require(_value <= allowance[_from][msg.sender]);
-
-        balances[_from] = balances[_from].sub(_value);
-
-        balances[_to] = balances[_to].add(_value);
-
-        // allowance[_from][msg.sender] = allowance[_from][msg.sender].sub(_value); 
-
-        emit Transfer( _from,  _to, _value);
+        emit Approval(msg.sender, _spender, _value);
 
         return true;
     }
 
-    // function generateAccount(string memory slackId) public returns(bool success) { 
-    //     toHash = 
-    
-    // }
-}
 
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+
+        require(_value <= balanceOf[_from]);
+
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] = balanceOf[_from].sub(_value);
+
+        balanceOf[_to] = balanceOf[_to].add(_value);
+
+        allowance[_from][msg.sender] = allowance[_from][msg.sender].sub(_value); 
+
+        emit Transfer(_from, _to, _value);
+
+        return true;
+    }
